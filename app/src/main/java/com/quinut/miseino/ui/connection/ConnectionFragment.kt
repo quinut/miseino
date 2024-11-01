9f566862c59414af4b76b0563b14f8ab36c48a04
quinut <66158094+quinut@users.noreply.github.com>
2024-11-01 23:20:09 +0900

    탭을전부고침

--- app/src/main/java/com/quinut/miseino/ui/connection/ConnectionFragment.kt
+++ app/src/main/java/com/quinut/miseino/ui/connection/ConnectionFragment.kt
@@ -1,17 +1,16 @@
-package com.quinut.miseino.ui.notifications
+package com.quinut.miseino.ui.connection
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.quinut.miseino.databinding.FragmentNotificationsBinding
+import com.quinut.miseino.databinding.FragmentConnectionBinding
 
-class NotificationsFragment : Fragment() {
+class ConnectionFragment : Fragment() {
 
-private var _binding: FragmentNotificationsBinding? = null
+private var _binding: FragmentConnectionBinding? = null
   // This property is only valid between onCreateView and
   // onDestroyView.
   private val binding get() = _binding!!
@@ -21,10 +20,10 @@ private var _binding: FragmentNotificationsBinding? = null
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
-    val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+    val connectionViewModel =
+            ViewModelProvider(this).get(ConnectionViewModel::class.java)
 
-    _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+    _binding = FragmentConnectionBinding.inflate(inflater, container, false)
     val root: View = binding.root
 
 
