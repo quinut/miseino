299972b379380f1fee3ef757a986dd6233ec2497
quinut <66158094+quinut@users.noreply.github.com>
2024-11-01 22:51:14 +0900

    Revert "원래대로"
    This reverts commit 5935bd101d6b0f0c18e305ba00e40c26093ab071.

--- app/src/main/java/com/quinut/miseino/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/quinut/miseino/ui/notifications/NotificationsViewModel.kt
@@ -1,10 +1,10 @@
-package com.quinut.miseino.ui.connection
+package com.quinut.miseino.ui.notifications
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ConnectionViewModel : ViewModel() {
+class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
